bc0b0cfc26120d8d6bb76fcdf333e387591ea200
Alex Childers <42629593+abchilders@users.noreply.github.com>
2020-02-27 10:22:20 -0800

    WIP - Parse Login and Navigation 10:50

--- app/src/main/java/com/example/loginactivity/LoginActivity.java
+++ app/src/main/java/com/example/loginactivity/LoginActivity.java
@@ -1,13 +1,19 @@
 package com.example.loginactivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 /**
  * A login screen that offers login via username/password.
@@ -44,6 +50,28 @@ public class LoginActivity extends AppCompatActivity {
     // takes a username and a password; logs in a user with these credentials
     private void loginUser(String username, String password) {
         Log.i(TAG, "Attempting to log in user" + username);
-        // TODO: navigate to the main activity if the user has signed in properly
+        // login in background so we're not blocking main thread for user
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                // if e is not null, something went wrong
+                if (e != null) {
+                    Log.e(TAG, "Issue with login", e);
+                    Toast.makeText(LoginActivity.this, "Issue with login!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                // navigate to the main activity if the user has signed in properly
+                goMainActivity();
+                Toast.makeText(LoginActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    // use Intent system to redirect to main activity
+    // TODO: LEFT OFF HERE-- NEED TO TEST LOGIN
+    private void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
     }
 }
--- app/src/main/java/com/example/loginactivity/ParseApplication.java
+++ app/src/main/java/com/example/loginactivity/ParseApplication.java
@@ -2,5 +2,19 @@ package com.example.loginactivity;
 
 import android.app.Application;
 
+import com.parse.Parse;
+
 public class ParseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Set applicationId, and server based on the value in the Heroku settings.
+        // clientKey is not needed unless explicitly configured
+        // any network interceptors must be added with the Configuration Builder given this syntax
+        Parse.initialize(new Parse.Configuration.Builder(this)
+            .applicationId("abchilders-finstagram") // should correspond to APP_ID env variable
+            .clientKey("OurFinstagramIsAShadyAsHellApp") // set explicitly unless clientKey is explicitly configured on Parse server
+            .server("http://abchilders-finstagram.herokuapp.com/parse").build());
+    }
 }
