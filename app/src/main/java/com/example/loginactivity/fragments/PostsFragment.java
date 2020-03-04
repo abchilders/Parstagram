5c4c7d5f888bf9c89eca1a52297670762c0318f2
Alex Childers <42629593+abchilders@users.noreply.github.com>
2020-03-04 09:57:12 -0800

    Complete "Posts RecyclerView"

--- app/src/main/java/com/example/loginactivity/fragments/PostsFragment.java
+++ app/src/main/java/com/example/loginactivity/fragments/PostsFragment.java
@@ -3,13 +3,26 @@ package com.example.loginactivity.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.loginactivity.Post;
+import com.example.loginactivity.PostsAdapter;
 import com.example.loginactivity.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -17,6 +30,10 @@ import com.example.loginactivity.R;
  */
 public class PostsFragment extends Fragment {
 
+    public static final String TAG = "PostsFragment";
+    private RecyclerView rvPosts;
+    private PostsAdapter adapter;
+    private List<Post> allPosts;
 
     public PostsFragment() {
         // Required empty public constructor
@@ -29,4 +46,42 @@ public class PostsFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_posts, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        rvPosts = view.findViewById(R.id.rvPosts);
+        allPosts = new ArrayList<>();
+        adapter = new PostsAdapter(getContext(), allPosts);
+
+        // Steps to use the recycler view:
+        // 0. create layout for one row in the list -- done in item_post.xml
+        // 1. create the adapter -- done above
+        // 2. create the data source -- done above
+        // 3. set the adapter on the recycler view
+        rvPosts.setAdapter(adapter);
+        // 4. set the layout manager on the recycler view
+        rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+        queryPosts();
+    }
+
+    private void queryPosts() {
+        // Specify which class to query
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                // if e is null, we have a list of posts to show
+                for (Post post : posts) {
+                    Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
+                }
+                allPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
 }
